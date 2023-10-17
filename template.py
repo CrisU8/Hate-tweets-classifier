339e58c0eacb504ddefddc4a097598c8fc4e3bda
CrisU8 <cristopher.urbina.h@ug.uchile.cl>
2023-10-17 12:31:31 -0300

    Estructura proyecto

--- template.py
+++ template.py
@@ -1,48 +1,50 @@
 import os
 from pathlib import Path
-import logging  
+import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-proyect_name = 'HATE_SPEECH_DETECTION'
+
+project_name = "textcls"
 
 list_of_files = [
-    'github/workflows/.gitkeep',
-    f'src/{proyect_name}/__init__.py',
-    f'src/{proyect_name}/components/__init__.py',
-    f'src/{proyect_name}/utils/__init__.py',
-    f'src/{proyect_name}/utils/common.py',
-    f'src/{proyect_name}/logging/__init__.py',
-    f'src/{proyect_name}/config/__init__.py',
-    f'src/{proyect_name}/config/configuration.py',
-    f'src/{proyect_name}/pipeline/__init__.py',
-    f'src/{proyect_name}/entities/__init__.py',
-    f'src/{proyect_name}/constants/__init__.py',
-    'config/config.yaml',
-    'dvc.yaml',   
-    'params.yaml',
-    'requirements.txt',
-    'setup.py',
-    'notebooks/trials.ipynb',
-    'reports/.gitkeep',
-    'templates/index.html',
-
-    ]
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir !="":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f'Creating directory {filedir} for file {filename}')
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, 'w') as f:
+        with open(filepath,'w') as f:
             pass
-        logging.info(f'Creating empty file {filepath}')
+            logging.info(f"Creating empty file: {filepath}")
+
+
     
     else:
-        logging.warning(f'File {filepath} already exists and is not empty')
-
+        logging.info(f"{filename} is already exists")
